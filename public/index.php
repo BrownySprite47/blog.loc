f4261d7760772f098416aa9eb2e2d04cd6862962
Anna Galochkina <kr0085@mail.ru>
2018-07-28 18:31:02 +0400

    Updating the file in index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,21 @@
 <?php
+
+define('ROOT_DIR', __DIR__);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Core\Router;
+
+
+try {
+    echo '<pre>';
+
+    $router = new Router();
+
+    $controller = $router->getController();
+
+    new $controller();
+
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}

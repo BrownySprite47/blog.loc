f6c0a9dde59f0c49deb3eb9c212d8d87056f9fde
Anna Galochkina <kr0085@mail.ru>
2018-08-04 23:51:34 +0400

    Create assets structure

--- public/index.php
+++ public/index.php
@@ -1,20 +1,7 @@
 <?php
 
-define('ROOT_DIR', __DIR__);
-define('LAYOUTS_DIR', __DIR__ . '/../app/views/layouts/');
+require_once __DIR__ . '/../app/Config/constants.php';
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Core\Router;
-
-
-try {
-    $router = new Router();
-    $controller = $router->getController();
-
-    $id = $router->getId();
-    new $controller($id);
-
-} catch (\Exception $e) {
-    echo $e->getMessage();
-}
+require_once __DIR__ . '/../app/bootstrap.php';

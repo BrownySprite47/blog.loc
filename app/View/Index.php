a7e52baafc67cbdde0279274ebee041fb24baf3e
Anna Galochkina <kr0085@mail.ru>
2018-08-04 23:53:13 +0400

    Add autoloas css, js, templates files

--- app/View/Index.php
+++ app/View/Index.php
@@ -6,8 +6,37 @@ use Core\AbstractView;
 
 class Index extends AbstractView
 {
+    protected $config;
+
+    protected $title;
+
+    protected $data;
+
+    protected $css;
+
+    protected $js;
+    /**
+     * @param $page
+     * @param array $data
+     * @throws \Exception
+     */
     public function view($page, $data = [])
     {
-        require_once __DIR__ . '/../views/templates' . $page . '.phtml';
+        $config = str_replace("/", "_", $page);
+        $this->config = require CONFIG_TEMPLATES_DIR .''. $config .'.php';
+
+        $this->title = $this->config[$page]['title'];
+        $this->data = $data;
+        $this->css = $this->config[$page]['css'];
+        $this->js = $this->config[$page]['js'];
+
+        foreach ($this->config[$page]['structure'] as $file)
+        {
+            if(file_exists($file)){
+                require_once $file;
+            }else{
+                throw new \Exception('Template file not found ' . $file);
+            }
+        }
     }
 }

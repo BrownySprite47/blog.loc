f2a7fa64c54672c51ed71293b2db72bcc65694bf
Anna Galochkina <kr0085@mail.ru>
2018-08-04 23:49:56 +0400

    Create constants config

---
+++ app/Config/constants.php
@@ -0,0 +1,15 @@
+<?php
+
+define('ROOT_DIR', __DIR__ . '/../../public/');
+
+define('CSS_DIR', '/assets/css/');
+
+define('JS_DIR', '/assets/js/');
+
+define('TEMPLATES_DIR', __DIR__ . '/../templates/');
+
+define('COMMON_TEMPLATES_DIR', __DIR__ . '/../templates/common/');
+
+define('CONFIG_TEMPLATES_DIR', __DIR__ . '/../templates/config/');
+
+define('CONFIG_DIR', __DIR__ . '/');
